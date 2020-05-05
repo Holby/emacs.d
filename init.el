b4315d39336168e67cd645d1d5194a47b36d86da
Björn Holby <bjorn.holby@gmail.com>
2020-05-05 21:19:51 +0200

    Adds archived packages

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("local" . "./packages/"))
 (setq package-enable-at-startup nil)
 (package-initialize)
 
